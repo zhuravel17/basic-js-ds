20b1ea72e1a581a8fd529cba74b6cc016f2fa68c
Zhuravel17 <kat.20030304@yandex.ru>
2025-01-06 17:46:55 +0400

    feat: add removeKFromList

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+ if (l.value === k) {
+  l = l.next;
+ }
+ let cur = l;
+ while (cur.next) {
+  if (cur.next.value === k) {
+    cur.next = cur.next.next;
+  } else {
+    cur = cur.next;
+  }
+ }
+ return l;
 }
 
 module.exports = {
